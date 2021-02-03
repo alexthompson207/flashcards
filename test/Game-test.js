4d451c6057b4b02993a73e61b7e84f5bdb78efef
Alex Thompson <ahthomps1@gmail.com>
2021-02-03 13:06:27 -0700

    Add function and instanceof test to game-test

---
+++ test/Game-test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Game = require('../src/Game');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
+describe('Game', () => {
+  let game;
+
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
+  it('should be a function', () => {
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be a an instance of Game', () => {
+    expect(game).to.be.an.instanceof(Game);
+  })
+
+})
